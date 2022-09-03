54548e920160fcef3c86218dbbb2f769f65d3c24
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-09-03 17:50:25 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function submit() {
-  hasil = document.getElementById("hasil").innerHTML = "";
-  var val = searchInput.value;
-  var newElement = document.createElement('script');
-  newElement.src = `https://www.googleapis.com/customsearch/v1?key=AIzaSyD69KZdQRASdg0QxpOA74adD4HeFRgHwx8&cx=e5dbd697a8e464044&q=${val}&callback=hndlr`;
-  newElement.id = "mainscript";
-  document.body.appendChild(newElement);
-}
