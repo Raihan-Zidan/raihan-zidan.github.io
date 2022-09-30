ac62262faa5972e4d0c7e6730079d834f79b16a3
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-09-30 19:11:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ var url = new URL(window.location.href);
 var q = url.searchParams.get("q");
 searchInput.value = q;
 
-function disableBack() { window.history.forward(); }
-setTimeout("disableBack()", 0);
-window.onunload = function () { null };
+if (!q || q === null) {
+  window.location.href = "/";
+}
 
 searchInput.addEventListener('keyup', ()=> {
   if (searchInput.value != '') {
@@ -24,9 +24,6 @@ window.addEventListener('load', ()=> {
     submit();
   } else {
     cleartext.style.display = "none";
-  }
-  if (!q || q === null || !searchInput.value) {
-    window.location.href = "/";
   }
   searchItem.forEach(tab => {
     if (tab.id === "all") {
