08ea4ae155617a4d8ade94aff3cb67aaabced25f
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-08 00:11:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -134,9 +134,14 @@ function instant(res) {
     if (res.Heading && res.Abstract && res.AbstractURL) {
       setTimeout(()=> {
       tabresult = document.querySelectorAll(".tab-result");
+      spellingText = document.querySelector(".spelling");
       instantbox = document.createElement("div");
       instantbox.innerHTML = `<div class="instant-answer"><img src="" alt="Icon" align="right" class="logo"><div class="title">${res.Heading}</div><div class="subtitle">${res.Entity}</div><div class="about"><span class="snippet">${TrimString(res.Abstract, 248)}</span><a href="${res.AbstractURL}" class="wikipedia" title="Wikipedia">Wikipedia</a></div><div class="infobox"></div></div>`;
-      insertBefore(tabresult[1], instantbox);
+      if (spelling) {
+        insertBefore(tabresult[1], instantbox);
+      } else {
+        insertBefore(tabresult[0], instantbox);
+      }
       if (res.Image) {
         document.querySelector(".instant-answer .logo").src = `https://duckduckgo.com${res.Image}`;
       } else {
