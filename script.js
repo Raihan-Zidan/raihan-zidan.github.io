22bf1461fc94791fbd15d2a5942d70e585e2391c
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-08 00:02:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -133,15 +133,10 @@ function instant(res) {
   try {
     if (res.Heading && res.Abstract && res.AbstractURL) {
       setTimeout(()=> {
-      tabresult = document.querySelectorAll(".tab-result")[0];
-      resultstats = document.querySelector(".result-stats");
+      tabresult = document.querySelectorAll(".tab-result")[1];
       instantbox = document.createElement("div");
       instantbox.innerHTML = `<div class="instant-answer"><img src="" alt="Icon" align="right" class="logo"><div class="title">${res.Heading}</div><div class="subtitle">${res.Entity}</div><div class="about"><span class="snippet">${TrimString(res.Abstract, 248)}</span><a href="${res.AbstractURL}" class="wikipedia" title="Wikipedia">Wikipedia</a></div><div class="infobox"></div></div>`;
-      if (!resultstats) {
-        insertBefore(tabresult, instantbox);
-      } else {
-        inserAfter(resultstats, instantbox);
-      }
+      insertBefore(tabresult, instantbox);
       if (res.Image) {
         document.querySelector(".instant-answer .logo").src = `https://duckduckgo.com${res.Image}`;
       } else {
