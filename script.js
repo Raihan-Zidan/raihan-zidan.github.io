7d6adb9b060411f86c90fddc59c5f18c4af907f6
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-09 15:43:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -106,10 +106,8 @@ function hndlr(res) {
           description.innerHTML = `There is no information on this page.`;
         }
       });
-    } else {
-      document.getElementById("hasil").innerHTML += `<div class="tab-result"><div class="title black">No matching results</div><div class="snippet suggestion">Search suggestions:</div><div class="snippet"><li>Try different keywords.</li><li>Try more general keywords.</li><li>Try fewer keywords.</li></div></div>`;
     }
   } catch(error) {
-      
+      document.getElementById("hasil").innerHTML += `<div class="tab-result"><div class="title black">No matching results</div><div class="snippet suggestion">Search suggestions:</div><div class="snippet"><li>Try different keywords.</li><li>Try more general keywords.</li><li>Try fewer keywords.</li></div></div>`;
   }
 }
