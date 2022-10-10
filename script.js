d38d64017bce301bbb40f6166bd062d8c8eb86f6
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-10 20:39:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function hndlr(res) {
       document.getElementById("hasil").innerHTML += `<div class="tab-result"><div class="snippet">Did you mean: <a class="spelling" href="/search?q=${res.spelling.correctedQuery}">${res.spelling.correctedQuery}</a></div></div>`;
     }
     for (var i = 0; i < res.items.length; i++) {
-      document.getElementById("hasil").innerHTML += `<div class="tab-result"><div class="tab-link"><a href="${res.items[i].link}"><div class="top"><img src="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${res.items[i].link}&size=64" class="favicon"><div class="link">${res.items[i].displayLink}</div></div><div class="title">${res.items[i].htmlTitle.replace(/\<\/?b.*?\/?\>/gi, "").replace("\u003ctextarea\u003e/", "&lt;\u003cb\u003etextarea\u003c/b\u003e&gt;"}</div></a></div><div class="snippet">${res.items[i].htmlSnippetreplace(/\<\/?b.*?\/?\>/gi, "")}</div></div>`;
+      document.getElementById("hasil").innerHTML += `<div class="tab-result"><div class="tab-link"><a href="${res.items[i].link}"><div class="top"><img src="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${res.items[i].link}&size=64" class="favicon"><div class="link">${res.items[i].displayLink}</div></div><div class="title">${res.items[i].htmlTitle.replace(/\<\/?b.*?\/?\>/gi, "").replace("\u003ctextarea\u003e/", "&lt;\u003cb\u003etextarea\u003c/b\u003e&gt;")}</div></a></div><div class="snippet">${res.items[i].htmlSnippetreplace(/\<\/?b.*?\/?\>/gi, "")}</div></div>`;
     }
     snippet = document.querySelectorAll(".snippet");
     snippet.forEach(description => {
