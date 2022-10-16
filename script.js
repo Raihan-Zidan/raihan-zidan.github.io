43eac05a8d0d23bc700b2e20b4be5b9927fbede4
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-16 10:45:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -111,3 +111,18 @@ function hndlr(res) {
     document.getElementById("hasil").innerHTML += `<div class="tab-result"><div class="title black">No matching results</div><div class="snippet suggestion">Search suggestions:</div><div class="snippet"><li>Try different keywords.</li><li>Try more general keywords.</li><li>Try fewer keywords.</li></div></div>`;
   }
 }
+
+iconImage = document.querySelectorAll("img");
+
+function convertImage(imageUrl, imageFile) {
+  fetch(imageUrl).then(function(response) {
+    return response.blob();
+  }).then(function(blobImage) {
+    var objectURL = URL.createObjectURL(blobImage);
+    imageFile.src = objectURL;
+  });
+}
+
+iconImage.forEach(icon => {
+  convertImage(icon.src, icon);
+});
