efb3a421ff5daa79e1699efb3918a9f43d6a6667
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-16 10:53:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -107,7 +107,6 @@ function hndlr(res) {
       }
     });
     document.getElementById("hasil").innerHTML = document.getElementById("hasil").innerHTML.replace(/\<\/?b.*?\/?\>/g, "");
-    },1000);
   } catch(error) {
     document.getElementById("hasil").innerHTML += `<div class="tab-result"><div class="title black">No matching results</div><div class="snippet suggestion">Search suggestions:</div><div class="snippet"><li>Try different keywords.</li><li>Try more general keywords.</li><li>Try fewer keywords.</li></div></div>`;
   }
