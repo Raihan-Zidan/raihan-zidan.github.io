176a0d250f0ca160fac367d2d234002c9f118fd4
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-20 00:27:01 +0700

    Update videos.js

--- videos.js
+++ videos.js
@@ -4,7 +4,7 @@ cleartext = document.querySelector(".cleartext");
 var url = new URL(window.location.href);
 var q = url.searchParams.get("q");
 searchInput.value = q;
-document.title.innerHTML = `${q} - Search`;
+document.title = `${q} - Search`;
 
 if (!q || q === null) {
   window.location.href = "/";
