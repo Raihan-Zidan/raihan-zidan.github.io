2d26fed33a2504c1af54a0dd7d450bf47a1f780d
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-20 18:21:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -83,10 +83,10 @@ searchApi = apikey[Math.floor(Math.random() * apikey.length)];
 function submit() {
   hasil = document.getElementById("hasil").innerHTML = "";
   var val = searchInput.value;
-  var newElement = document.createElement('script');
-  newElement.src = `https://www.googleapis.com/customsearch/v1?key=${searchApi}&cx=e5dbd697a8e464044&q=${val}&callback=hndlr`;
-  newElement.id = "mainscript";
-  document.head.appendChild(newElement);
+  fetch(`https://www.googleapis.com/customsearch/v1?key=${searchApi}&cx=e5dbd697a8e464044&q=${val}`)
+    .then(response => response.json()).then(response => {
+      hndlr(response);
+  })
 }
 
 function hndlr(res) {
