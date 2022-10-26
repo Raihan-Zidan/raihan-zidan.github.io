4c7203d34a3386af896eaa5c67451d3433b616f9
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-26 19:23:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ var url = new URL(window.location.href);
 var q = url.searchParams.get("q");
 searchInput.value = q;
 document.title = `${q} - Search`;
+var p = url.searchParams.get("p");
+var startIndex = 1;
+if (p === 2) {
+  startIndex = 21;
+} else if (p === 3) {
+  startIndex = 31;
+} else if (p === 4) {
+  startIndex = 41;
+}
 
 if (!q || q === null) {
   window.location.href = "/";
@@ -22,7 +31,7 @@ searchInput.addEventListener('keyup', ()=> {
 window.addEventListener('load', ()=> {
   if (searchInput.value != '') {
     cleartext.style.display = "block";
-    submit();
+    setTimeout(()=> { submit(); },500);
   } else {
     cleartext.style.display = "none";
   }
@@ -83,7 +92,7 @@ searchApi = apikey[Math.floor(Math.random() * apikey.length)];
 function submit() {
   hasil = document.getElementById("hasil").innerHTML = "";
   var val = searchInput.value;
-  fetch(`https://www.googleapis.com/customsearch/v1?key=${searchApi}&cx=e5dbd697a8e464044&q=${val}`)
+  fetch(`https://www.googleapis.com/customsearch/v1?key=${searchApi}&start=${startIndex}&cx=e5dbd697a8e464044&q=${val}`)
     .then(response => response.json()).then(response => {
       hndlr(response);
   })
