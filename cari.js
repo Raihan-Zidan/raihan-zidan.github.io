1fc2812218c5ebceefe171633b1496a88a2e56d7
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-28 00:25:28 +0700

    Update cari.js

--- cari.js
+++ cari.js
@@ -3,7 +3,11 @@ var url = new URL(window.location.href);
 var q = url.searchParams.get("q");
 document.title = `${q} - Search`;
 var startIndex = 1;
-document.body.innerHTML += `<div class="header"><div class="search-box"><div class="search-field"><input class="search-input" value="${q}" autocorrect="off" autocomplete="off" autocapitalize="off" placeholder="Type to search..."><div role="button" class="cleartext"></div></div></div><div class="search-menu"><div class="search-item"><a href="" class="tab-wrapper" id="all"><img src="images/search.svg"><span>All</span></a></div><div class="search-item"><a href="" class="tab-wrapper" id="images"><img src="images/image.svg"><span>Images</span></a></div><div class="search-item"><a href="" class="tab-wrapper" id="videos"><img src="images/video.svg"><span>Videos</span></a></div><div class="search-item"><a href="" class="tab-wrapper" id="maps"><img src="images/maps.svg"><span>Maps</span></a></div></div></div><div id="hasil"></div>`;
+if (q) {
+  document.body.innerHTML += `<div class="header"><div class="search-box"><div class="search-field"><input class="search-input" value="${q}" autocorrect="off" autocomplete="off" autocapitalize="off" placeholder="Type to search..."><div role="button" class="cleartext"></div></div></div><div class="search-menu"><div class="search-item"><a href="" class="tab-wrapper" id="all"><img src="images/search.svg"><span>All</span></a></div><div class="search-item"><a href="" class="tab-wrapper" id="images"><img src="images/image.svg"><span>Images</span></a></div><div class="search-item"><a href="" class="tab-wrapper" id="videos"><img src="images/video.svg"><span>Videos</span></a></div><div class="search-item"><a href="" class="tab-wrapper" id="maps"><img src="images/maps.svg"><span>Maps</span></a></div></div></div><div id="hasil"></div>`;
+} else {
+  window.location.href = "/";
+}
 searchInput = document.querySelector(".search-input");
 searchItem = document.querySelectorAll(".tab-wrapper");
 cleartext = document.querySelector(".cleartext");
@@ -12,9 +16,6 @@ if (tbm === "vid") {
   document.querySelectorAll(".search-item")[2].classList.add("selected");
 } else {
   document.querySelectorAll(".search-item")[0].classList.add("selected");
-}
-if (!q) {
-  window.location.href = "/";
 }
 
 searchInput.addEventListener('keyup', ()=> {
