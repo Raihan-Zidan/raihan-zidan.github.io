240861a09998f27363327c3d492b3aad7a538c38
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-27 22:05:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -108,7 +108,9 @@ function hndlr(res) {
       }
     });
     document.getElementById("hasil").innerHTML = document.getElementById("hasil").innerHTML.replace(/\<\/?b.*?\/?\>/g, "");
-    if (res.items.length = 10) { moreresult() }
+    if (res.items.length > 9) {
+      moreresult()
+    }
   } catch(error) {
     document.getElementById("hasil").innerHTML += `<div class="tab-result"><div class="title black">No matching results</div><div class="snippet suggestion">Search suggestions:</div><div class="snippet"><li>Try different keywords.</li><li>Try more general keywords.</li><li>Try fewer keywords.</li></div></div>`;
   }
