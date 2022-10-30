553e94eb5c211f25a236d675ce37bcb31905d528
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-10-30 21:53:18 +0700

    Update cari.js

--- cari.js
+++ cari.js
@@ -9,6 +9,11 @@ if (q) {
   window.location.href = "/";
 }
 
+var translate = document.createElement("meta");
+translate.name = "google";
+translate.content = "notranslate";
+document.head.appendChild(translate);
+
 searchInput = document.querySelector(".search-input");
 searchItem = document.querySelectorAll(".tab-wrapper");
 cleartext = document.querySelector(".cleartext");
