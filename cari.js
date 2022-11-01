1f8c4992b72d650a7bc70d2b5294c62afea32dd3
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-01 19:47:10 +0700

    Update cari.js

--- cari.js
+++ cari.js
@@ -165,7 +165,3 @@ function moreresult() {
     submit();
   }
 }
-
-function searcherror() {
-  document.getElementById("hasil").innerHTML += `<div class="tab-result"><div class="title black">No matching results</div><div class="snippet suggestion">Search suggestions:</div><div class="snippet"><li>Try different keywords.</li><li>Try more general keywords.</li><li>Try fewer keywords.</li></div></div>`;
-}
