5748e1de7e60a0c2c59965225e800892d84c67cd
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-04 23:50:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ function webresult(res) {
     document.querySelector(".main-result").innerHTML = document.querySelector(".main-result").innerHTML.replace(/\<\/?b.*?\/?\>/g, "");
     setTimeout(()=> {
       document.querySelector(".main-result").innerHTML += `<div class="more-wrapper"><button class="more">Show more</button></div>`;
-    }
+    },1000);
   } catch(error) {
     document.querySelector(".main-result").innerHTML += `<div class="tab-result"><div class="title black">No matching results</div><div class="snippet suggestion">Search suggestions:</div><div class="snippet"><li>Try different keywords.</li><li>Try more general keywords.</li><li>Try fewer keywords.</li></div></div>`;
   }
