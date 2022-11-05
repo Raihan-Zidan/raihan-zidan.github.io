b5de84a5fb87d9cff32a2dfe02d98135af63a195
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-05 16:36:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ function webresult(res) {
 }
 
 function moreresult() {
-  document.querySelector(".show-wrapper").innerHTML = `<div class="loader"><svg class="circular" viewBox="25 25 50 50"><circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10"/></svg></div>`;
+  document.querySelector(".show-wrapper").innerHTML = `<div class="loader"><svg class="circular" viewBox="25 25 50 50"><circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="4" stroke-miterlimit="10"/></svg></div>`;
   if (startIndex < 20) {
     startIndex += 10;
     setTimeout(()=> { submit(); }, 500);
