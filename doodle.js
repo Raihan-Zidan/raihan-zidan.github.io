899e0dd7a7ec8a6ecb19962ccfa06fe36fa67916
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-05 12:01:23 +0700

    Update doodle.js

--- doodle.js
+++ doodle.js
@@ -1,4 +1,4 @@
 var doodle = {
-  "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT38V_w6LkpOF37Mts3QblVvdO5pgSkNujhA&s",
+  "image": "images/logo.png",
   "default": "images/logo.png"
 };
