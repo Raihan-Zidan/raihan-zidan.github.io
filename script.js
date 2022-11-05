efe25ae5b0b6327bd7e2e4e9c4c54e284d63e316
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-05 09:27:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -157,18 +157,12 @@ function webresult(res) {
     });
     document.querySelector(".main-result").innerHTML = document.querySelector(".main-result").innerHTML.replace(/\<\/?b.*?\/?\>/g, "");
     if (res.queries.nextPage) {
-      document.querySelector(".main-result").innerHTML += `<div class="show-wrapper"><button class="more">Show more</button></div>`;
+      document.querySelector(".main-result").innerHTML += `<div class="show-wrapper"><button class="more" onclick="moreresult()">Show more</button></div>`;
     }
   } catch(error) {
     document.querySelector(".main-result").innerHTML += `<div class="tab-result"><div class="title black">No matching results</div><div class="snippet suggestion">Search suggestions:</div><div class="snippet"><li>Try different keywords.</li><li>Try more general keywords.</li><li>Try fewer keywords.</li></div></div>`;
   }
 }
-
-var showmore = document.querySelector(".show-wrapper .more");
-
-showmore.addEventListener('click', ()=> {
-  moreresult();
-});
 
 function moreresult() {
   startIndex += 10;
