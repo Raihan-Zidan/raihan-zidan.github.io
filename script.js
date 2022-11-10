2087dd1514da22d103082ec795fbd4ecd39f060b
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-10 19:42:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -64,8 +64,7 @@ searchInput.addEventListener('keyup', ()=> {
 });
 
 searchToggle.addEventListener('click', ()=> {
-  var searchdata = "";
-  searchdata = (tbm === "vid") ? "&tbm=vid" : searchdata;
+  var searchdata = (tbm === "vid") ? "&tbm=vid" : searchdata;
   if (searchInput.value.trim()) {
     window.location.href = `/search?q=${encodeURIComponent(searchInput.value).replace(/\%20/g,'+')}${searchdata}`;
   }
