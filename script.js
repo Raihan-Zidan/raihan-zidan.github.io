a92c1e3b08f5b7f4771ff077f14f5e0e212673ec
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-12 08:05:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ function submit() {
 function videoresult(res) {
   try {
     for (var i = 0; i < res.items.length; i++) {
-      document.querySelector(".main-result").innerHTML += `<div class="video-result"><a href="https://youtube.com/watch?v=${res.items[i].id.videoId}"  data-number="1"><img src="${res.items[i].snippet.thumbnails.medium.url}" class="thumbnail"><div class="title">${res.items[i].snippet.title}</div><div class="source"><div class="info"><img src="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://www.youtube.com&size=64" class="favicon"><div>www.youtube.com</div></div></div></a></div>`;
+      document.querySelector(".main-result").innerHTML += `<div class="video-result"><a href="https://youtube.com/watch?v=${res.items[i].id.videoId}"  data-number="1"><img src="${res.items[i].snippet.thumbnails.medium.url}" class="thumbnail"><div class="title">${res.items[i].snippet.title}</div><div class="source"><div class="info"><img src="images/youtube.png" class="favicon"><div>www.youtube.com</div></div></div></a></div>`;
     }
     if (!res.items.length > 0) throw "empty";
   } catch(error) {
