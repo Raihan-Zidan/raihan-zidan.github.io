ce33854f43a241f2b5916e02fa6ae5144e5b2f08
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-17 14:21:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -143,15 +143,15 @@ function videoresult(res) {
 }
 
 function nwsresult(res) {
-  var tabres = document.querySelectorAll(".tab-result")[3];
+  var tabres = document.querySelectorAll(".tab-result");
   setImeout(()=> {
     var nwsres = document.createElement("div");
     nwsres.innerHTML += `<div class="news-result"><div class="title">News result</div><div class="news-list"></div></div>`;
-    insertAfter(tabres, nwsres);
+    insertAfter(tabres[3], nwsres);
     for (var i = 0; i < res.items.length; i++) {
       document.querySelector(".news-result .news-list").innerHTML += `<div class="news-tab"><a href="${res.items[i].link}"><img class="thumbnail" src="${res.items[i].pagemap.cse_thumbnail[0].src}"><div class="title">${res.items[i].title}</div></a></div>`;
     }
-  },1000);
+  },1800);
 }
 
 function webresult(res) {
