9cfc5dd39cf52fe031780472de6179514fd8e18a
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-17 19:41:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function submit() {
         webresult(response);
     })
     if (startIndex == 1) {
-    fetch(`https://www.googleapis.com/customsearch/v1/siterestrict?key=${searchApi}&cx=c0eb0b8c9dc2143c9&q=${val}`)
+    fetch(`https://www.googleapis.com/customsearch/v1?key=${searchApi}&cx=c0eb0b8c9dc2143c9&q=${val}`)
       .then(response => response.json()).then(response => {
         nwsresult(response);
     })
