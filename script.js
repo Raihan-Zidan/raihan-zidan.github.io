3148c8e569e00cc1540f40ef093482343830db9d
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-19 07:43:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ function submit() {
       .then(response => response.json()).then(response => {
         webresult(response);
     })
-    if (Math.floor(Math.random() * 3) == 1 && startIndex = 1) {
+    if (Math.floor(Math.random() * 3) == 1 && startIndex == 1) {
     fetch(`https://www.googleapis.com/customsearch/v1?key=${searchApi}&cx=c0eb0b8c9dc2143c9&q=${val}`)
       .then(response => response.json()).then(response => {
         nwsresult(response);
