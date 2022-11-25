6baf78e71fb58396b02d28da83268d546fa3e320
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-25 19:44:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -195,6 +195,7 @@ function videoresult(res) {
     }
     if (!res.items.length > 0) throw "empty";
   } catch(error) {
+    document.querySelector(".main-result").style.minWidth = "0";
     document.querySelector(".main-result").innerHTML += `<div class="tab-result"><div class="title-black">${langtext("noresult")}</div><div class="suggestion">${langtext("suggtext")}</div><div>${langtext("noresultsug")}"</div></div>`;
   }
 }
