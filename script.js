4ead3521c6deac0b96cec1426680d48aa7f718b1
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-11-29 09:21:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ var clock = function() {
 
 function instantanswer() {
   c = ["clock","jam"];
-  if (searchInput.value.toLowerCase().match(/(c[0]|c[1])/) && searchInput.value.length < 15 && searchInput.value.split(" ").length - 4) {
+  if (searchInput.value.toLowerCase().match(/c[0]|c[1]/) && searchInput.value.length < 15 && searchInput.value.split(" ").length - 4) {
     document.querySelector(".main-result .result").innerHTML += `<div class="tab-result"><div class="big-title">${clock()}</div><div class="snippet-info">${d.toLocaleDateString(locallang, {weekday: 'long',year: 'numeric',month: 'long',day: 'numeric'})} (${d.toLocaleDateString(locallang, {timeZoneName: 'short'}).substr(-3)}) ${d.toString().match(/([A-Z]+[\+-][0-9]+.*)/)[1]}</div></div>`;
   }
 }
