b355d95ecbc2ecb6ab870fbfcd4eea7dc9e77bc1
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2022-12-07 20:46:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -219,11 +219,12 @@ var clock = function() {
 }
 
 function instantanswer() {
-  c = ["clock","jam"];
-  if (searchInput.value.toLowerCase().match(c[0]) && searchInput.value.length < 15 && searchInput.value.split(" ").length - 4 || searchInput.value.toLowerCase().match(c[1]) && searchInput.value.length < 15 && searchInput.value.split(" ").length - 4) {
+  if (searchInput.value.toLowerCase().match("clock") && searchInput.value.length < 15 && searchInput.value.split(" ").length - 4 || searchInput.value.toLowerCase().match("jam") && searchInput.value.length < 15 && searchInput.value.split(" ").length - 4) {
     document.querySelector(".main-result .result").innerHTML += `<div class="tab-result"><div class="big-title">${clock()}</div><div class="snippet-info">${d.toLocaleDateString(locallang, {weekday: 'long',year: 'numeric',month: 'long',day: 'numeric'})} (${d.toLocaleDateString(locallang, {timeZoneName: 'short'}).substr(-3)}) ${d.toString().match(/([A-Z]+[\+-][0-9]+.*)/)[1]}</div></div>`;
   } else if (searchInput.value.toLowerCase().match("tanggal") && searchInput.value.length < 15 && searchInput.value.split(" ").length - 4) {
     document.querySelector(".main-result .result").innerHTML += `<div class="tab-result"><div class="big-title">${d.toLocaleDateString(locallang, {weekday: 'long',year: 'numeric',month: 'long',day: 'numeric'})}</div></div>`;
+  } else if (searchInput.value.match("indonesia") && searchInput.value.split(" ").length - 2) {
+    document.querySelector(".main-result .result").innerHTML += `<div class="tab-result"><div class="big-title">Indonesia</div><div class="snippet">Country in Southeast Asia</div></div>`;
   }
 }
 
