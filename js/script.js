2422fb687ce613327a0e9df1161e883818088303
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-04-02 11:50:08 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+var weburl = "https://raihan-zidan.github.io";
+
+inputBox.onkeyup = (e)=> {
+  let userData = e.target.value;
+  let emptyArray = [];
+  if (userData) {
+    icon.onclick = ()=>{
+      webLink = `${weburl}/search?q=${userData}`;
+      linkTag.setAttribute("href", webLink);
+      linkTag.click();
+    }
+    emptyArray = suggestions.filter((data)=>{
+      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+    });
+    emptyArray = emptyArray.map((data)=>{
+      return data = `<li>${data}</li>`;
+    });
+    searchWrapper.classList.add("active");
+    showSuggestions(emptyArray);
+    let allList = suggBox.querySelectorAll("li");
+    for (let i = 0; i < allList.length; i++) {
+      allList[i].setAttribute("onclick", "select(this)");
+    }
+  } else {
+    searchWrapper.classList.remove("active");
+  }
+}
+
+function select(element) {
+  let selectData = element.textContent;
+  inputBox.value = selectData;
+  webLink = `${weburl}/search?q=${selectData}`;
+  linkTag.setAttribute("href", webLink);
+  linkTag.click();
+  searchWrapper.classList.remove("active");
+}
+
+inputBox.addEventListener('blur', ()=> {
+  setTimeout(()=> {
+    searchWrapper.classList.remove("active");
+  },50);
+});
+
+function showSuggestions(list) {
+  let listData;
+  if (!list.length) {
+    userValue = inputBox.value;
+    listData = `<li>${userValue}</li>`;
+    searchWrapper.classList.remove("active");
+  } else {
+    listData = list.join('');
+  }
+  suggBox.innerHTML = listData;
+}
