5c2c83595c1e9632c719fc7c094f5060ee97e713
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-05-06 15:50:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -310,10 +310,10 @@ function relatedsearch() {
   fetch(`https://api.swisscows.com/suggest?query=${q}`)
   .then(response => response.json()).then(response => {
     for (var i = 1; i < response.length && i < 5; i++) {
-      if (q) {
+      if (response.length > 3) {
         document.querySelector(".main-result .result").appendChild(rltb);
         rltb.innerHTML = `<div class="title">Related search</div><div class="search-list">`;
-        document.querySelector(".search-list").innerHTML += `<a href="/search?q=${response[i]}" class="related">${response[i]}</a>`;
+        setTimeout(()=> {document.querySelector(".search-list").innerHTML += `<a href="/search?q=${response[i]}" class="related">${response[i]}</a>`;},100);
       }
     }
   })
