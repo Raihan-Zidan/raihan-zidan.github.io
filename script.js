8a16bcf8eb8ee289c5c67987933bc2b16dec8cea
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-05-06 15:46:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -310,7 +310,7 @@ function relatedsearch() {
   fetch(`https://api.swisscows.com/suggest?query=${q}`)
   .then(response => response.json()).then(response => {
     for (var i = 1; i < response.length && i < 5; i++) {
-      if (i > 3) {
+      if (q) {
         document.querySelector(".main-result .result").appendChild(rltb);
         rltb.innerHTML = `<div class="title">Related search</div><div class="search-list">`;
         document.querySelector(".search-list").innerHTML += `<a href="/search?q=${response[i]}" class="related">${response[i]}</a>`;
