ee391d7a61d57d15be380fc11c2796c511505253
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-05-07 10:29:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -358,7 +358,7 @@ function timeAgo(input) {
 function nwsr(res) {
   try {
     for (var i = 0; i < res.items.length; i++) {
-      publisher = (res.items[i].pagemap.newsmediaorganization[0].name) ? res.items[i].pagemap.newsmediaorganization[0].name : res.items[i].displayLink;
+      publisher = (res.items[i].pagemap.newsmediaorganization) ? res.items[i].pagemap.newsmediaorganization[0].name : res.items[i].displayLink;
       publishtime = (res.items[i].pagemap.metatags) ? res.items[i].pagemap.metatags[0]["article:published_time"] : "Published";
       document.querySelector(".main-result").innerHTML += `<div class="tab-result nwst"><div class="snwt"><a href="${res.items[i].link}"><img class="thumb" align="right" src="${res.items[i].pagemap.cse_thumbnail[0].src}"><div class="top"><img src="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${res.items[i].link}&size=64" class="favicon"><div class="link">${publisher}</div></div><div class="title">${res.items[i].title}</div><div class="publishtime">${publishtime}</div></a></div></div>`;
     }
