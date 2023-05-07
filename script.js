024c1cd99b9db082b84137435cb1f7e893d0d6fe
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-05-07 10:01:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -334,6 +334,27 @@ function refreshQuotes() {
   },400);
 }
 
+function timeAgo(input) {
+  const date = (input instanceof Date) ? input : new Date(input);
+  const formatter = new Intl.RelativeTimeFormat('en');
+  const ranges = {
+    years: 3600 * 24 * 365,
+    months: 3600 * 24 * 30,
+    weeks: 3600 * 24 * 7,
+    days: 3600 * 24,
+    hours: 3600,
+    minutes: 60,
+    seconds: 1
+  };
+  const secondsElapsed = (date.getTime() - Date.now()) / 1000;
+  for (let key in ranges) {
+    if (ranges[key] < Math.abs(secondsElapsed)) {
+      const delta = secondsElapsed / ranges[key];
+      return formatter.format(Math.round(delta), key);
+    }
+  }
+}
+
 function nwsr(res) {
   try {
     for (var i = 0; i < res.items.length; i++) {
