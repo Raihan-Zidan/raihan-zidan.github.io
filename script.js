62cf202959567e9718391321389b8753c1684319
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-05-07 20:16:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -358,7 +358,8 @@ function nwsr(res) {
   try {
     for (var i = 0; i < res.items.length; i++) {
       publisher = (res.items[i].pagemap.metatags[0]['og:site_name']) ? res.items[i].pagemap.metatags[0]['og:site_name'] : res.items[i].displayLink;
-      publishtime = (res.items[i].pagemap.metatags[0]['article:published_time']) ? timeAgo(res.items[i].pagemap.metatags[0]['article:published_time']) : "Published";
+      publishtimes = (res.items[i].pagemap.metatags[0]['article:published_time'].slice(0, 4) == d.getFullYear()) timeAgo(res.items[i].pagemap.metatags[0]['article:published_time']) : new Date(res.items[i].pagemap.metatags[0]['article:published_time'].slice(0, 10)).toLocaleString('en-US', {day: '2-digit', year: 'numeric', month: 'long'}
+      publishtime = (res.items[i].pagemap.metatags[0]['article:published_time']) ? publishtimes : "Published";
       newssnippet = (windowWidth > 780) ? `<div class="snippet">${res.items[i].snippet}</div>` : "";
       thumbimg = (res.items[i].pagemap.cse_thumbnail) ? `<img class="thumb" align="right" src="${res.items[i].pagemap.cse_thumbnail[0].src}">` : "";
       document.querySelector(".main-result").innerHTML += `<div class="tab-result nwst"><div class="snwt"><a href="${res.items[i].link}">${thumbimg}<div class="top"><img src="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${res.items[i].link}&size=64" class="favicon"><div class="link">${publisher}</div></div><div class="title">${res.items[i].title.slice(0, 70)}</div>${newssnippet}<div class="publishtime">${publishtime}</div></a></div></div>`;
