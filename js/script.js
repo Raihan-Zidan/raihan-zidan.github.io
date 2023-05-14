486b74cd0da386c01e778f5fff466f82a1b5c1a8
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-05-14 18:14:51 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,27 @@
 const searchWrapper = document.querySelector(".search-input");
-
 const inputBox = searchWrapper.querySelector("input");
-
 const suggBox = searchWrapper.querySelector(".autocom-box");
+var url = new URL(window.location.href);
+var hl = url.searchParams.get("hl");
+var lang = (hl == "id") ? "&hl=id" : "&hl=en";
 
 inputBox.addEventListener('keyup', ()=> {
-
   var query = inputBox.value;
-
-  suggBox.innerHTML = "";
+  if (!inputBox.value) {
+    suggBox.innerHTML = "";
+  }
   searchWrapper.classList.remove("active")
-
   fetch(`https://api.swisscows.com/suggest?query=${inputBox.value}`)
-
     .then(response => response.json())
-
     .then(response => {
-
       for (var i = 1; i < response.length; i++) {
-
+        suggBox.innerHTML = "";
         suggBox.innerHTML += `<li>${response[i]}</li>`;
         searchWrapper.classList.add("active")
-
       }
-
     });
-
 });
+
+if (hl == "id") {
+  inputBox.placeholder = "Ketik untuk mencari...";
+}
