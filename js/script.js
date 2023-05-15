ce3766bff60dc67e7305633a5a49c87c842026ca
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-05-15 13:47:19 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,123 +1,67 @@
 const searchWrapper = document.querySelector(".search-input");
-
 const inputBox = searchWrapper.querySelector("input");
-
 const suggBox = searchWrapper.querySelector(".autocom-box");
-
 const icon = searchWrapper.querySelector(".icon");
-
 const daylabel = document.querySelector(".url a");
-
 var url = new URL(window.location.href);
-
 var hl = url.searchParams.get("hl");
-
 var lang = (hl == "id") ? "&hl=id" : "&hl=en";
-
 let linkTag = searchWrapper.querySelector("a");
-
 let webLink;
-
 var weburl = "https://raihan-zidan.github.io";
 
 inputBox.onkeyup = (e)=> {
-
   let userData = e.target.value;
-
   let emptyArray = [];
-
   if (userData) {
-
     icon.onclick = ()=>{
-
       webLink = `${weburl}/search?q=${inputBox.value}${lang}`;
-
       linkTag.setAttribute("href", webLink);
-
       linkTag.click();
-
     }
-
     emptyArray = suggestions.filter((data)=>{
-
       return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-
     });
-
     emptyArray = emptyArray.map((data)=>{
-
       return data = `<li>${data}</li>`;
-
     });
-
     searchWrapper.classList.add("active");
-
     showSuggestions(emptyArray);
-
     let allList = suggBox.querySelectorAll("li");
-
     for (let i = 0; i < allList.length; i++) {
-
       allList[i].setAttribute("onclick", "select(this)");
-
     }
-
   } else {
-
     searchWrapper.classList.remove("active");
-
   }
-
   if (e.keyCode == 13) {
-
     let selectData = inputBox.value;
-
     webLink = `${weburl}/search?q=${selectData}${lang}`;
-
     linkTag.setAttribute("href", webLink);
-
     linkTag.click();
-
   }
-
 }
 
 inputBox.onfocus = (e)=> {
-
   userData = e.target.value;
-
   if (userData) {
-
     searchWrapper.classList.add("active");
-
   }
-
 }
 
 function select(element) {
-
   let selectData = element.textContent;
-
   inputBox.value = selectData;
-
   webLink = `${weburl}/search?q=${selectData}${lang}`;
-
   linkTag.setAttribute("href", webLink);
-
   linkTag.click();
-
 }
 
 document.body.addEventListener('click', (e)=> {
-
     elm = e.target;
-
     if (!elm.classList.contains("active") && !elm.classList.contains("autocom-box") && !elm.classList.contains("input")) {
-
       searchWrapper.classList.remove("active");
-
     }
-
 });
 
 inputBox.addEventListener('keyup', (e)=> {
@@ -125,35 +69,23 @@ inputBox.addEventListener('keyup', (e)=> {
 });
 
 function showSuggestions(list) {
-
   let listData;
-
   if (!list.length) {
-
     userValue = inputBox.value;
-
     listData = `<li>${userValue}</li>`;
-
     searchWrapper.classList.remove("active");
-
   } else {
-
     listData = list.join('');
-
   }
-
   suggBox.innerHTML = listData;
-
 }
 
 if (hl == "id") {
-
   inputBox.placeholder = "Ketik untuk mencari...";
-
+  document.title = "Mesin pencari";
 }
 
 daylabel.innerHTML = (hl == "id") ? "Penerjemah" : "Translator";
-
 daylabel.href = `/translator`;
 
 var url = `/search?q=${daylabel.innerHTML}`;
