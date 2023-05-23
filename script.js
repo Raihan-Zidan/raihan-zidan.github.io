8db6933d231c03149820f9182e4204f7581f6ec4
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-05-23 19:35:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -497,3 +497,55 @@ window.addEventListener('load', ()=> {
     },1000)
   }
 });
+
+
+
+const calculatorBox = document.querySelector(".calculator");
+
+if (calculatorBox) {
+
+const display = calculatorBox.querySelector(".display");
+
+const buttons = calculatorBox.querySelectorAll("button");
+
+const specialChars = ["%", "*", "/", "-", "+", "="];
+
+let output = "";
+
+const calculate = (btnValue) => {
+
+  display.focus();
+
+  if (btnValue === "=" && output !== "") {
+
+    output = eval(output.replace("%", "/100"));
+
+  } else if (btnValue === "AC") {
+
+    output = "";
+
+  } else if (btnValue === "DEL") {
+
+    output = output.toString().slice(0, -1);
+
+  } else {
+
+    if (output === "" && specialChars.includes(btnValue)) return;
+
+    output += btnValue;
+
+  }
+
+  display.value = output;
+
+  display.blur();
+
+};
+
+buttons.forEach((button) => {
+
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+
+});
+
+}
