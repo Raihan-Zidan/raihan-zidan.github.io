4e6fbc3a11ce31356f2ace3d08dc4ecb95b8e3d8
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-06-08 19:54:21 +0700

    Create sitelinks.js

---
+++ sitelinks.js
@@ -0,0 +1,41 @@
+[{
+	"site": "http://www.google.com/",
+	"links": [
+		["Gmail", "https://mail.google.com/mail/?tab=wm", "In your Google Account, you can see and manage your information"],
+		["Search", "https://www.google.com/webhp", "Search the world's information, including webpages, images, videos and more..."],
+		["Meet Bard", "https://bard.google.com/", "Bard is your creative and helpful collaborator to supercharge..."],
+		["News", "https://news.google.com/?tab=wn", "News - Google"]
+	]
+}, {
+	"site": "https://www.microsoft.com/",
+	"links": [
+		["Account", "https://account.microsoft.com/account", "Sign In with your Microsoft account. One account. One place to manage it all."],
+		["Microsoft 365", "https://www.microsoft.com/en-us/microsoft-365", "Introducing Microsoft 365 Copilot The new Microsoft 365 Copilot experience..."],
+		["Office", "https://www.microsoft.com/en-us/microsoft-365/microsoft-office", "Microsoft 365 is your powerful cloud-based productivity platform that includ..."],
+		["Contact Us", "https://support.microsoft.com/en-us/contactus", "Contact Microsoft Support. Find solutions to common problems, or get..."]
+	]
+}, {
+	"site": "https://www.ecosia.org/?c=en",
+	"links": [
+		["How Ecosia Works", "https://info.ecosia.org/what", "Search ads generate income for Ecosia. Ecosia uses this income to plant trees..."],
+		["Privacy", "https://info.ecosia.org/privacy", "Ecosia is a “privacy friendly” search engine. We take user..."],
+		["Blog", "https://blog.ecosia.org/", "News from Ecosia, the alternative search engine that plants..."],
+		["Tree Store", "https://plant.ecosia.org/", "Join the Ecosia community by following us on social media and see progress and…"]
+	]
+}, {
+	"site": "https://duckduckgo.com/",
+	"links": [
+		["Start Page", "https://start.duckduckgo.com", "Start Page - DuckDuckGo — Privacy, simplified."],
+		["About", "https://duckduckgo.com/about", "DuckDuckGo is the Internet privacy company for everyone who's ..."],
+		["All Settings", "https://duckduckgo.com/settings", "Settings - DuckDuckGo — Privacy, simplified."],
+		["Install Duckduckgo", "https://duckduckgo.com/install", "Set DuckDuckGo as your default search engine. 1. Right click in the search bar."]
+	]
+}, {
+	"site": "https://www.minecraft.net/en-us",
+	"links": [
+		["Login", "https://www.minecraft.net/en-us/login", "Sign in to your account with your Minecraft login information and..."],
+		["Games", "https://www.minecraft.net/en-us/about-minecraft", "Minecraft is a sandbox video game developed by Mojang Studios. The game allows..."],
+		["Minecraft Legends", "https://www.minecraft.net/en-us/about-legends", "Minecraft Legends is an action strategy game developed by Mojang Studios and Blackbird Interactive."],
+		["Download", "https://www.minecraft.net/en-us/download", "Download Minecraft for Windows, Mac and Linux. Get server..."]
+	]
+}]
