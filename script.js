fc832444c9685f8070f945f13b8794d57d6847c0
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-06-09 09:58:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -630,7 +630,7 @@ function webresult(res) {
 }
 
 function shwfter() {
-  document.querySelector(".rcthl-wrp").insertAdjacentHTML('beforeend', `<section class="footer"><ul class="list"><li><a href="/settings">Settings</a></li><li><a href="/">Privacy</a></li><li><a href="/search?q=translate">Translate</a></li></ul><div class="copyright">©Copyright 2023</div></section>`);
+  document.querySelector(".rcthl-wrp").insertAdjacentHTML('beforeend', `<section class="footer"><ul class="list"><li><a href="/settings">Settings</a></li><li><a href="/">Privacy</a></li><li><a href="javascript:share();">Share</a></li><li><a href="/search?q=translate">Translate</a></li></ul><div class="copyright">©Copyright 2023</div></section>`);
 }
 
 function share() {
