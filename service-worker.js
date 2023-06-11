a33add7c784b5f50dd3847c6a1a80c0d676becd6
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-06-11 18:53:24 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,32 @@
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      cacheNames.forEach(function(cacheName) {
-        console.log('Nama Cache:', cacheName);
-        // Anda dapat memeriksa apakah cache tersebut berisi offline page
-        // dan menghapusnya jika perlu
-      });
+self.addEventListener('fetch', function(event) {
+  if (!navigator.onLine) {
+    event.respondWith(
+      new Response("Tidak ada koneksi internet. Silakan cek kembali nanti.", {
+        headers: { 'Content-Type': 'text/plain' }
+      })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
+});
+
+self.addEventListener('offline', function(event) {
+  self.clients.matchAll().then(function(clients) {
+    clients.forEach(function(client) {
+      client.postMessage({ type: 'offline', message: 'Tidak ada koneksi internet. Silakan cek kembali nanti.' });
+    });
+  });
+});
+
+self.addEventListener('online', function(event) {
+  self.clients.matchAll().then(function(clients) {
+    clients.forEach(function(client) {
+      client.postMessage({ type: 'online' });
+    });
+  });
 });
