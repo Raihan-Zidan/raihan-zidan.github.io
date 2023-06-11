429f0be5c8b36b86e856c941b608c3a5c63f65dd
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-06-11 17:35:06 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,12 @@
 var CACHE_NAME = 'my-cache';
 
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
-});
-
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.filter(function(cacheName) {
-          return cacheName !== CACHE_NAME;
+          // Hapus cache yang tidak diperlukan lagi
+          return cacheName !== 'my-cache';
         }).map(function(cacheName) {
           return caches.delete(cacheName);
         })
@@ -18,14 +15,3 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return fetch(event.request).then(function(response) {
-        cache.delete(event.request.url);
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    })
-  );
-});
