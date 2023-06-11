8064e297cbce71def7de71c897ae3df076c455b2
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-06-11 08:34:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -561,7 +561,7 @@ function hnvd(res) {
   var videonya = "";
   var a = 8;
   var b = a + 4;
-  for (var i = 0; i < res.items.length && i < 5; i++) {
+  for (var i = a; i < res.items.length && i < b; i++) {
     videonya += `<div class="vidbung">
       <div class="tab-link"><a href="https://youtube.com/watch?v=${res.items[i].id.videoId}"><div class="viditem">
         <div class="thumbnail">
