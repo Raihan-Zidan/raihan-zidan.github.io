3ca16744adbbda73f81aad9be2c3907f97690fa8
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-06-14 23:03:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,28 @@ if (tbm === "vid") {
   insertAfter(defstyle, vidstyle);
 }
 
-
+var language = {
+  en: {
+    news: "News result",
+    more: "Show more",
+    placeholder: "Type to search...",
+    correct: "Did you mean:",
+    noresult: "No matching results",
+    suggtext: "Search suggestion:",
+    noresultsug: ["Try different keywords.", "Try more general keywords.", "Try fewer keywords."],
+    tab: ["All","Images","Videos","News","Maps"],
+  },
+  id: {
+    news: "Hasil berita",
+    more: "Lihat lainnya",
+    placeholder: "Ketik untuk  mencari...",
+    correct: "Apakah yang kamu maksud:",
+    noresult: "Tidak ditemukan hasil",
+    suggtext: "Saran pencarian:",
+    noresultsug: ["Coba kata kunci yang berbeda.", "Coba kata kunci yang lebih umum.", "Coba lebih sedikit kata kunci."],
+    tab: ["Semua","Gambar","Video","Berita","Peta"],
+  },
+};
 
 var sitelinkss;
 
