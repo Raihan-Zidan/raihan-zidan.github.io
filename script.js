7308802f8ba4ae5d40fe6938a2393910792646e5
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2023-06-14 20:25:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -546,7 +546,7 @@ function hnvd(res) {
   if (tabres[0] && q.split(" ").length < 3) {
     insertAfter(tabres[0], hnvde);
   } else if (tabres[0]) {
-    document.querySelector(".main-result .result").insertAdjacentHTML("afterbegin", hnvde);
+    document.querySelector(".main-result .result").insertAdjacentHTML("afterbegin", `<div class="tab-result Dxcgd">${hnvde.innerHTML}</div>);
   }
  }
 }
