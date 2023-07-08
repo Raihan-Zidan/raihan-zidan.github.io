6dfd358da6f6a33f1d75fb7c6dbb00b8a1ae4940
raaihanzr <136738746+raaihanzr@users.noreply.github.com>
2023-07-08 20:22:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -724,7 +724,7 @@ function webresult(res) {
     }
     var newsKey = ['chrome', 'youtube', 'twitter', 'google', 'microsoft', 'duckduckgo'];
     if (newsKey.includes(q.trim().toLowerCase()) && pageone) {
-      fetch(`https://www.googleapis.com/customsearch/v1?key=${searchApi}&sort=date&cx=1428d6f56512346f2&q=${q}`)
+      fetch(`https://www.googleapis.com/customsearch/v1?key=${searchApi}&sort=date&cx=1428d6f56512346f2&q=${q}&exactTerms={q}`)
         .then(response => response.json()).then(response => {
           nwsresult(response);
       })
