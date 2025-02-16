fe66acab1d72fd03d474fcc41affe459b2f82f7d
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2025-02-16 09:41:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -678,7 +678,7 @@ function webresult(res) {
       displayUrl = res.items[i].displayLink;
       var wSnippet = (res.items[i].snippet) ? res.items[i].snippet : "";
       console.log((res.items[i].snippet) ? res.items[i].snippet : "");
-      var siteName = (res.items[i].pagemap.metatags[0]['og:site_name']) ? res.items[i].pagemap.metatags[0]['og:site_name'] : displayUrl;
+      var siteName = res.items[i]?.pagemap?.metatags?.[0]?.['og:site_name'] ?? displayUrl;
       document.querySelector(".main-result .result").insertAdjacentHTML('beforeend', `<div class="VtuHV Kj7VF tab-result" ${fdta}><div class="tab-link"  data-number="${i}"><a href="${res.items[i].link}"><div class="top"><div class="favicon"><img src="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://${originurl.hostname}&size=64"></div><div class="link-rw"><div class="link">${siteName}</div><div class="link k">https://${res.items[i].displayLink}</div></div></div><div class="title">${res.items[i].title}</div></a></div><div class="btm-snpt"><div class="snippet">${wSnippet}</div>${showLinks(res.items[i].link)}</div></div>`);
     }
     snippet = document.querySelectorAll(".snippet");
