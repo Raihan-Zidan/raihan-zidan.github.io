26056a6680842075c7dad811ff90406c789adcfa
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2025-02-16 09:23:01 +0700

    Create playstore.js

---
+++ playstore.js
@@ -0,0 +1,48 @@
+      if (q.toLowerCase() == "games" && windowWidth < 780 && i == 1 && pageone) {
+        document.querySelector(".main-result .result").insertAdjacentHTML('beforeend', `<div class="VtuHV tab-result pL6sR">
+      <span class="atas-euy"><div class="top">
+      <img class="favicon" src="img/google-play.png">
+      <div class="link">Google Play</div>
+    </div></span><div class="MciPl">
+  <div class="tab-link">
+  <a href="https://play.google.com/store/apps/details?id=com.fingersoft.hcr2&hl=id_ID">
+     <div class="information">
+      <img src="https://play-lh.googleusercontent.com/2fdEgYhRnpPRTSJLcJ7CpZ9-dFcfbNrP-BYVzzUoCwXyVBMRMVtBUrce0k_l-dV62ps=w480-h960-rw">
+      <div class="label">
+         <div class="title notranslate">Hill Climb Racing 2</div>
+         <div class="snippet">Rating 4.6 (4M)</div>
+         <div class="snippet">Balapan • Stunt driving • Arkade</div>
+      </div>
+    </div>
+  </a>
+  </div>
+  <hr>
+    <div class="tab-link">
+  <a href="https://play.google.com/store/apps/details?id=com.mojang.minecraftpe&hl=id_ID">
+
+     <div class="information">
+      <img src="https://play-lh.googleusercontent.com/VSwHQjcAttxsLE47RuS4PqpC4LT7lCoSjE7Hx5AW_yCxtDvcnsHHvm5CTuL5BPN-uRTP=w480-h960-rw">
+      <div class="label">
+         <div class="title notranslate">Minecraft</div>
+         <div class="snippet">Rating 4.6 (4.8M)</div>
+         <div class="snippet">Simulasi • Sandbox • Santai</div>
+      </div>
+    </div>
+  </a>
+  </div>
+  <hr>
+    <div class="tab-link">
+  <a href="https://play.google.com/store/apps/details?id=com.imangi.templerun2&hl=id_ID">
+
+     <div class="information">
+      <img src="https://play-lh.googleusercontent.com/go4XqS4mYs-G2tZymiVLF4wJYXIi5QrvwixNRzssk4G_vRBHrAdg4E1ddNwy9c2cZA=w480-h960-rw">
+      <div class="label">
+         <div class="title notranslate">Temple Run 2</div>
+         <div class="snippet">Rating 4.3 (10M)</div>
+         <div class="snippet">Aksi • Platformer • Berlari • Arkade</div>
+      </div>
+    </div>
+  </a>
+  </div></div>
+</div>`);
+      }
