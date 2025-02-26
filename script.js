f28f1268b3d513b5c271e895a15c0a8c4c3ec46c
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2025-02-26 18:04:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -318,7 +318,7 @@ function submit() {
         videoresult(response);
     })
   } else if (tbm == "nws") {
-    fetch(`https://datasearch.raihan-zidan2709.workers.dev/api?q=${q}&hl=id&tbm=nws`)
+    fetch(`https://imagesearch.raihan-zidan2709.workers.dev/news?q=${q}`)
       .then(response => response.json()).then(response => {
         nwsr(response);
     })
@@ -506,11 +506,11 @@ function dateconversion(val) {
 function nwsr(res) {
   try {
     for (var i = 0; i < res.items.length; i++) {
-      publisher = (res.items[i].pagemap.metatags[0]['og:site_name']) ? res.items[i].pagemap.metatags[0]['og:site_name'] : res.items[i].displayLink;
-      var publishtime = (res.items[i].pagemap.metatags[0]['article:published_time']) ? dateconversion(res.items[i].pagemap.metatags[0]['article:published_time']) : "Published";
+      publisher = (res.items[i].source) ? res.items[i].source : res.items[i].url;
+      var publishtime = (res.items[i].posttime) ? res.items[i].posttime : "Published";
       newssnippet = (windowWidth > 780) ? `<div class="snippet">${res.items[i].snippet}</div>` : "";
-      thumbimg = (res.items[i].pagemap.cse_thumbnail) ? `<img class="thumb" src="${res.items[i].pagemap.cse_thumbnail[0].src}">` : "";
-      document.querySelector(".main-result").innerHTML += `<div class="tab-result nwst eb8xCva"><div class="snwt"><a href="${res.items[i].link}">${thumbimg}<div class="top"><img src="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${res.items[i].link}&size=64" class="favicon"><div class="link">${publisher}</div></div><div class="title">${res.items[i].title.slice(0, 70)}</div>${newssnippet}<div class="publishtime">${publishtime}</div></a></div></div>`;
+      thumbimg = (res.items[i].thumbnail) ? `<img class="thumb" src="${res.items[i].thumbnail}">` : "";
+      document.querySelector(".main-result").innerHTML += `<div class="tab-result nwst eb8xCva"><div class="snwt"><a href="${res.items[i].url}">${thumbimg}<div class="top"><img src="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${res.items[i].url}&size=64" class="favicon"><div class="link">${publisher}</div></div><div class="title">${res.items[i].title}</div>${newssnippet}<div class="publishtime">${publishtime}</div></a></div></div>`;
     }
     if (startIndex == 1) { shwfter(); }
   } catch(error) {
