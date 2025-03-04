0a20e4a633ffcb79d8ac6161b38b5c2307144c6c
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2025-03-04 11:23:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -659,13 +659,14 @@ function webresult(res) {
       urlparam = urlparam.endsWith(" › ") ? urlparam.slice(0, -3) : urlparam;
       urlparam = originurl.origin + urlparam;
 
+      var hasPri = (res.items[i]?.pagemap?.offer.[0]?.price) ? res.items[i]?.pagemap?.offer.[0]?.price : '';
       var fdta = `tab-num="${i}" data-test="awokwok" data-ved="0" isMobile="${isMobile}" data-sx="maacaa-cihh"`;
       displayUrl = res.items[i].displayLink;
       var siteName = res.items[i]?.pagemap?.metatags?.[0]?.['og:site_name'] ?? displayUrl;
       var bigThumb = (isMobile && res.items[i]?.pagemap?.metatags?.[0]?.['og:type'] == "website") ? '' : '';
 
       var isThumb = (!bigThumb && res.items[i]?.snippet?.length > 150 && res.items[i]?.pagemap?.cse_thumbnail) ? `<div class="thumbnail"><img src="${res.items[i].pagemap.cse_thumbnail[0].src}"></div>` : "";
-           document.querySelector(".main-result .result").insertAdjacentHTML('beforeend', `<div class="VtuHV Kj7VF tab-result eb8xCva" ${fdta}><div class="CeWka NbkAw"><div class="tab-link"  data-number="${i}"><a href="${res.items[i].link}"><div class="top"><div class="favicon"><img src="https://datasearch.searchdata.workers.dev/favicon?url=https://${originurl.hostname}"></div><div class="link-rw"><div class="link">${siteName}</div><div class="link k">https://${res.items[i].displayLink}</div></div></div><div class="title">${res.items[i].title}</div></a></div>${bigThumb}<div class="btm-snpt"><div class="snippet"><span>${res.items[i].snippet || langtext("noSiteInfo")}</span></div>${showLinks(res.items[i].link)}</div></div></div>`);
+           document.querySelector(".main-result .result").insertAdjacentHTML('beforeend', `<div class="VtuHV Kj7VF tab-result eb8xCva" ${fdta}><div class="CeWka NbkAw"><div class="tab-link"  data-number="${i}"><a href="${res.items[i].link}"><div class="top"><div class="favicon"><img src="https://datasearch.searchdata.workers.dev/favicon?url=https://${originurl.hostname}"></div><div class="link-rw"><div class="link">${siteName}</div><div class="link k">https://${res.items[i].displayLink}</div></div></div><div class="title">${res.items[i].title}</div></a></div>${bigThumb}<div class="btm-snpt"><div class="snippet"><span>${res.items[i].snippet || langtext("noSiteInfo")}</span></div>${hasPri}${showLinks(res.items[i].link)}</div></div></div>`);
     }
     if (getData().favicon == false || fv == 0) {
       document.querySelectorAll(".favicon").forEach(elm => {
